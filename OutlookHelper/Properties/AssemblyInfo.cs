43d14113d56d01ea8e3939c3b281c6397b7ffff0
T800 <25018911+T800G@users.noreply.github.com>
2022-05-11 15:24:15 +0200

    Update AssemblyInfo.cs

--- OutlookHelper/Properties/AssemblyInfo.cs
+++ OutlookHelper/Properties/AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration ("")]
 [assembly: AssemblyCompany ("")]
 [assembly: AssemblyProduct ("OutlookHelper")]
-[assembly: AssemblyCopyright ("T800 Productions (c) 2021")]
+[assembly: AssemblyCopyright ("T800 Productions (c) 2022")]
 [assembly: AssemblyTrademark ("")]
 [assembly: AssemblyCulture ("")]
 // This sets the default COM visibility of types in the assembly to invisible.
